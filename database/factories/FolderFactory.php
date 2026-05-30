1938f65788162d6e9a22c33db4a6fab633bb09fc
Ozan Kurt <me@ozankurt.com>
2026-05-30 09:44:00 +0300

    chore: rename namespace Database\Factories\Kurt\Modules\Library to Kurt\Modules\ResourceLibrary

--- database/factories/FolderFactory.php
+++ database/factories/FolderFactory.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Database\Factories\Kurt\Modules\Library;
+namespace Database\Factories\Kurt\Modules\ResourceLibrary;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Kurt\Modules\Library\Enums\FolderVisibility;
-use Kurt\Modules\Library\Models\Folder;
+use Kurt\Modules\ResourceLibrary\Enums\FolderVisibility;
+use Kurt\Modules\ResourceLibrary\Models\Folder;
 
 /**
  * @extends Factory<Folder>
